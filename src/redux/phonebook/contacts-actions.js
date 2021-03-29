fd62387d7a62f9a5a40ecf0b03b1550a22c92e81
Dm3583 <pankovdm@gmail.com>
2021-03-29 17:42:21 +0300

    refactoring

--- src/redux/phonebook/contacts-actions.js
+++ src/redux/phonebook/contacts-actions.js
@@ -1,18 +1,8 @@
+import { createAction } from '@reduxjs/toolkit';
 import types from './contacts-types';
 
-const addContact = contact => ({
-  type: types.ADD,
-  payload: contact,
-});
-
-const deleteContact = contactId => ({
-  type: types.DELETE,
-  payload: contactId,
-});
-
-const changeFilter = value => ({
-  type: types.CHANGE_FILTER,
-  payload: value,
-});
+const addContact = createAction(types.ADD);
+const deleteContact = createAction(types.DELETE);
+const changeFilter = createAction(types.CHANGE_FILTER);
 
 export default { addContact, deleteContact, changeFilter };
